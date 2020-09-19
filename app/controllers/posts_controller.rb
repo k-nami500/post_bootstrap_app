49860b7ba12c431a06eae788c4e500ef410236aa
Katsuya Minami <arms.mc.rp@gmail.com>
2020-09-19 18:19:25 +0900

    コントローラの処理を before_action で共通化

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,11 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: %i[show edit update destroy]
   def index
     @posts = Post.order(id: :asc)
   end
 
   def show
-    @post = Post.find(params[:id])
+    
   end
 
   def new
@@ -17,23 +18,27 @@ class PostsController < ApplicationController
   end
 
   def edit
-    @post = Post.find(params[:id])
+    
   end
 
   def update
-    post = Post.find(params[:id])
-    post.update!(post_params)
+    @post.update!(post_params)
     redirect_to posts_path
   end
 
   def destroy
-    post = Post.find(params[:id])
-    post.destroy!
+    @post.destroy!
     redirect_to posts_path
   end
 
   private
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
   def post_params
     params.require(:post).permit(:title, :content)
   end
+
 end
